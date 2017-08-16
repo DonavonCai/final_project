476ca8f2c0d974845a071480fe77af7c4af00dad
DonavonCai <donavoncai@yahoo.com>
2017-08-16 12:11:11 -0700

    Add files via upload
    Completed modal window for signup page.

--- recipes.com/js/scripts.js
+++ recipes.com/js/scripts.js
@@ -36,33 +36,24 @@ $('#signup-a').on('click', function () {
 // When the checkboxes for breakfast, lunch/dinner, or dessert is clicked
 // All dishes without the corresponding class will disappear
 // ============================================================================
-// Modal window for signup page ===============================================
-// When the submit button is clicked
-// Modal window appears
-// Rest of page darkens
-$('form').on('submit', function(event) {
-  event.preventDefault();
-
-});
-// ============================================================================
 // Logic for form submission ==================================================
-// Dates:
-$('#day').on('click', function() {
+var form_complete = false;
+var name_complete = false;
+var date_complete = false;
+var email_complete = false;
+var password_complete = false;
+$('body').on('click', function() {
+  // Days in month:
   var month=$('#month').val();
   var year=$('#year').val();
-  if (month == "") {
-    $('#month-alert').text('Please select a month.');
-  }
-  if (year == "") {
-    $('#year-alert').text('Please select a year.');
-  }
+  var day=$('#day').val();
   if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
     $("#day option[value='31']").show();
     $("#day option[value='30']").show();
     $("#day option[value='29']").show();
   } else if (month == 4 || month == 6 || month == 9 || month == 11) {
     $("#day option[value='31']").remove();
-  } else if (month ==2 && year%4 != 0) {
+  } else if (month == 2 && year%4 != 0 || month == 2 && year == "") {
     $("#day option[value='31']").hide();
     $("#day option[value='30']").hide();
     $("#day option[value='29']").hide();
@@ -72,39 +63,106 @@ $('#day').on('click', function() {
     $("#day option[value='29']").show();
   }
 });
-// Password/ Email matching:
+// Password/ email:
 $('form').on('submit', function () {
-  $('#email-match-alert').text("");
+  var first_name=$('#firstname').val();
+  var last_name=$('#lastname').val();
+  var month=$('#month').val();
+  var year=$('#year').val();
+  var day=$('#day').val();
+  var password1 = $('#password-enter').val();
+  var password2 = $('#password-confirm').val();
   var email1 = $('#user-email').val();
   var email2 = $('#user-email-confirm').val();
+  // Hide alerts on form resubmission:
+  $('#first-name-alert').text("");
+  $('#last-name-alert').text("");
+  $('#month-alert').text("");
+  $('#year-alert').text("");
+  $('#day-alert').text("");
+  $('#email-match-alert').text("");
+  $('#password-match-alert').text("");
+  // Name:
+  if (first_name == "") {
+    $('#first-name-alert').text('Please enter your first name.');
+  }
+  if (last_name == "") {
+    $('#last-name-alert').text('Please enter your last name.');
+  }
+  if (first_name != "" && last_name != "") {
+    name_complete = true;
+  }
+  // Date:
+  if (month == "") {
+    $('#month-alert').text('Please select a month.');
+  }
+  if (year == "") {
+    $('#year-alert').text('Please select a year.');
+  }
+  if (day == "") {
+    $('#day-alert').text('Please select a day.');
+  }
+  if (month != "" && year != "" && day != "") {
+    date_complete = true;
+  }
+  // Email:
   if (email1 == "") {
-    $('#email-alert').text('Please enter an email address.')
+    $('#email-alert').text('Please enter an email address.');
   } else if (email2 == "") {
     $('#email-alert').hide();
-    $('#email-confirm-alert').text('Please confirm your email address.')
+    $('#email-confirm-alert').text('Please confirm your email address.');
   } else if (email1 != email2) {
     $('email-confirm-alert').hide();
-    $('#email-match-alert').text('Email does not match.')
+    $('#email-match-alert').text('Email does not match.');
   } else {
     $('#email-confirm-alert').hide();
     $('#email-match-alert').hide();
   }
-});
-$('form').on('submit', function () {
-  $('#password-match-alert').text("");
-  var password1 = $('#password-enter').val();
-  var password2 = $('#password-confirm').val();
+  if (email1 != "" && email2 != "" && email1 == email2) {
+    email_complete = true;
+  }
+  // Password:
   if (password1 == "") {
-    $('#password-alert').text('Please enter a password.')
+    $('#password-alert').text('Please enter a password.');
   } else if (password2 == "") {
     $('#password-alert').hide();
-    $('#password-confirm-alert').text('Please confirm your password.')
+    $('#password-confirm-alert').text('Please confirm your password.');
   } else if (password1 != password2) {
     $('#password-confirm-alert').hide();
-    $('#password-match-alert').text('Passwords do not match.')
+    $('#password-match-alert').text('Passwords do not match.');
   } else {
     $('#password-confirm-alert').hide();
     $('#password-match-alert').hide();
+  }
+  if (password1 != "" && password2 != "" && password1 == password2) {
+    password_complete = true;
+  }
+});
+// Form completion:
+$('form').on('submit', function(event) {
+  event.preventDefault();
+if (name_complete == true && date_complete == true && email_complete == true && password_complete == true) {
+  form_complete = true;
+}
+if (form_complete == true) {
+
+}
+});
+// ============================================================================
+// Modal window for signup page ===============================================
+// When the submit button is clicked
+// Modal window appears
+// Rest of page darkens
+$('.modal-x').on('click', function (event) {
+  $('.modal-window').fadeOut(300);
+});
+$('.modal-button').on('click', function (event) {
+  var confirmation_code = $('#confirmation-code').val();
+  if (confirmation_code ==  "") {
+    event.preventDefault();
+  }
+  else {
+    window.open("confirmation.html");
   }
 });
 // ============================================================================
